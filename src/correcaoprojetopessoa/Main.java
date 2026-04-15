66c1a7cd00a13d94d74adb06f9c43631802d5250
daos12 <daos_ptc@hotmail.com>
2026-04-14 21:56:03 -0300

    inicialização classe main

--- src/correcaoprojetopessoa/Main.java
+++ src/correcaoprojetopessoa/Main.java
@@ -1,5 +1,23 @@
 package correcaoprojetopessoa;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    
+    Scanner s = new Scanner(System.in);
+    
+    //Lista de objetos
+    ArrayList<Pessoa> lista = new ArrayList<>();
+    int opcao=6;
+    
+    do{
+        System.out.println("1. Cadastro de Pessoas");
+        System.out.println("2. Listar Pessoas");
+        System.out.println("3. Buscar Pessoas");
+    
+    
+    
+    }while(opcao != 3);
     
 }
